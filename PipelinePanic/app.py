be57cf62bdae61080c4bb913e9cbdc3c7c980dda
jdsi23 <jameslusk535@gmail.com>
2025-05-16 09:48:14 -0400

    Update app.py

--- PipelinePanic/app.py
+++ PipelinePanic/app.py
@@ -292,5 +292,7 @@ def end():
                           turns=turns,
                           wrong_choices=wrong_choices)
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+if __name__ == "__main__":
+    import os
+    port = int(os.environ.get("PORT", 8080))
+    app.run(host="0.0.0.0", port=port)
